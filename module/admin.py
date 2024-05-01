06e691ea65adca274c35d4ff8ab24ddc552bd733
Kamal Ibrahimi <aquaburst001@gmail.com>
2024-05-01 13:16:41 +0500

    Add the followings:
    - Include Course, Module and Registration models in module/models.py
    - Register the above mentioned models in module/admin.py

--- module/admin.py
+++ module/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from module.models import ContactUs
+from module.models import ContactUs, Course, Module, Registration
 # Register your models here.
 
 
@@ -7,4 +7,26 @@ class ContactUsAdmin(admin.ModelAdmin):
     list_display = ('subject', 'email', 'message')
 
 
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name', 'description')
+
+
+class ModuleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'code', 'credit', 'category', 'description', 'availability', 'custom_method')
+    list_filter = ['availability']
+    search_fields = ('name', 'code', 'credit', 'category', 'description')
+
+    def custom_method(self, obj):
+        return ", ".join([related_obj.name for related_obj in obj.courses.all()])
+
+
+class RegistrationAdmin(admin.ModelAdmin):
+    list_display = ('student', 'module', 'registration_date')
+
+
+
 admin.site.register(ContactUs, ContactUsAdmin)
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Module, ModuleAdmin)
+admin.site.register(Registration, RegistrationAdmin)
--- module/models.py
+++ module/models.py
@@ -11,3 +11,50 @@ class ContactUs(models.Model):
     class Meta:
         db_table = 'ContactUs'
         ordering = ['-id']
+
+
+class Course(models.Model):
+    name = models.CharField(verbose_name='Course Name', max_length=100, db_column='course_name')
+    description = models.TextField(db_column='course_description', verbose_name='Course Description')
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        db_table = 'Course'
+
+
+
+AVAILABILITY_CHOICES = {
+    "Open": "Open",
+    "Closed": "Closed"
+}
+
+class Module(models.Model):
+    name = models.CharField(max_length=150, db_column='name', verbose_name='Name')
+    code = models.CharField(max_length=20, db_column='code', verbose_name='Code', unique=True)
+    credit = models.PositiveIntegerField(db_column='credit', verbose_name='Credit')
+    category = models.CharField(max_length=50, db_column='category', verbose_name='Category')
+    description = models.TextField(db_column='description', verbose_name='Description')
+    availability = models.CharField(max_length=15, choices=AVAILABILITY_CHOICES, default='Open')
+    courses = models.ManyToManyField("Course", related_name='modules', db_column='courses', verbose_name='Courses')
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        db_table = 'Module'
+
+
+class Registration(models.Model):
+    student = models.ForeignKey("student.Student", on_delete=models.CASCADE, related_name='registrations')
+    module = models.ForeignKey(Module, on_delete=models.CASCADE, related_name='registrations')
+    registration_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.student.user.username} registered for {self.module.name} on {self.registration_date}"
+
+    class Meta:
+        db_table = 'Registration'
+        unique_together = ('student', 'module')
+        ordering = ['-registration_date']
